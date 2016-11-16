b0c8a754c7c4e104a4e16dabcf63f05ccfb3a30b
estaciwo <staciwo@gmail.com>
2016-11-15 23:45:50 -0800

    Add files via upload

---
+++ ten-three-one/footer.php
@@ -0,0 +1,8 @@
+<div id="footer">
+    <cite>
+        &copy; <?php echo date('Y');?><a href="<?php bloginfo('url');?>">nookhouses </a>.All rights reserved.
+    </cite>
+</div>
+<?php wp_footer(); ?>
+</div>
+</html>

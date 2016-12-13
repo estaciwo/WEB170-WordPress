3ea44357fa80068db6fb3ebcc3511ecee760a4e7
estaciwo <staciwo@gmail.com>
2016-12-12 18:10:35 -0800

    Add files via upload

---
+++ erictheme/404.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?> 
+<div class="entry" id="404">
+    <p>Interesting...that page doesn't exist.</p>
+    <p>Feel free to search for something else:</p>
+    <p><?php get_search_form(); ?></p>
+</div>
+<?php get_footer(); ?>
---
+++ erictheme/footer.php
@@ -0,0 +1,12 @@
+<div id="footer">
+	<cite>
+		&copy; <?php echo date('Y');?><a href="<?php bloginfo('url');?>"> Eric Staciwo. </a>All rights reserved.
+
+
+	</cite>
+
+</div>
+<?php wp_footer(); ?>
+</div>
+</div>
+</html>
